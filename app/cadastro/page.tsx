d1b9450f925579c06ca8e7a456e544a360c64657
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-07-29 12:49:29 +0100

    Update page.tsx

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import "../auth.css";
 
+
 export default function Cadastro() {
   const [lang, setLang] = useState<"pt" | "en" | "es">("pt");
   const [nome, setNome] = useState("");
